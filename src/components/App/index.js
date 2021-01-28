f5497eb3b5634c58aba626c3e9750872c02f7c42
Christian Acosta <cta@christiantacosta.com>
2021-01-28 02:06:53 -0500

    init commit

---
+++ src/components/App/index.js
@@ -0,0 +1,13 @@
+import './styles.scss';
+import Navbar from '../Navbar';
+function App() {
+  return (
+    <div className="App">
+      <Navbar/>
+      Hello WallStreetBets
+
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Navbar/index.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import './styles.scss'
+function Navbar() {
+    return (
+        <div>
+            Navbar
+        </div>
+    )
+}
+
+export default Navbar
